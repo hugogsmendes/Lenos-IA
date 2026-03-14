2e5b1a46386e85e0bca70225212ccbd3c34a740d
Hugo Mendes <hugogs.mendes@gmail.com>
2026-03-14 19:33:48 -0300

    entidade usuarios

---
+++ models/users.py
@@ -0,0 +1,25 @@
+from utils.database import Base
+from uuid import UUID, uuid4
+from datetime import datetime
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User (Base):
+
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid = True), primary_key = True, default = uuid4)
+    name: Mapped[str] = mapped_column(String, nullable = False)
+    email: Mapped[str] = mapped_column(String, nullable = False, unique = True)
+    password_hash: Mapped[str] = mapped_column(String, nullable = False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        nullable = False)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        onupdate = func.now(),
+        nullable = False)
