481cee5a20ee83cd9cc5f7669e13b29f3110e8d9
Hugo Mendes <hugogs.mendes@gmail.com>
2026-03-17 20:25:30 -0300

    Sessions dos dois BD (redis e supabase)

---
+++ sessions/session_database.py
@@ -0,0 +1,16 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+from utils.database import SessionLocal
+from fastapi import Depends
+from repository.user_repository import User_Repository
+from service.user_service import User_Service
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
+
+def get_user_repository(session: AsyncSession = Depends(get_session)):
+    return User_Repository(session = session)
+
+def get_user_service(repository: User_Repository = Depends(get_user_repository)):
+    return User_Service(repository = repository)
