9b42bb0532f351789c6c686c345c56a77218cd09
Hugo Mendes <hugogs.mendes@gmail.com>
2026-03-17 20:24:30 -0300

    Teste de conexão com o BD

--- utils/database.py
+++ utils/database.py
@@ -1,27 +1,34 @@
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import create_engine
-import os 
 from dotenv import load_dotenv
+import os 
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+import asyncio
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
                     
-engine = create_engine(DATABASE_URL, pool_pre_ping = True)
+engine = create_async_engine(DATABASE_URL,
+                             echo = True, 
+                             pool_pre_ping = True,)
 
 Base = declarative_base() 
 
-SessionLocal = sessionmaker(bind = engine) 
+SessionLocal = async_sessionmaker(bind = engine, 
+                                  class_ = AsyncSession,
+                                  expire_on_commit = False) 
 
 if __name__ == "__main__":
 
-    def test_connect():
+    async def test_connect():
 
         try:
-            with engine.connect() as connection:
+            async with engine.connect() as conn:
                 return "Connection successful!"
         except Exception as e:
             print(f"Failed to connect: {e}")
+        finally:
+            await engine.dispose()
             
-    assert test_connect() == "Connection successful!"
+    assert asyncio.run(test_connect()) == "Connection successful!"
 
