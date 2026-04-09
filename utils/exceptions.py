ecf402a2b18e52b2a4f38b1c696c82baef990286
Hugo Mendes <hugogs.mendes@gmail.com>
2026-04-09 11:17:17 -0300

    fix: padrões nos status codes e exceções

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,28 +1,28 @@
 from fastapi import HTTPException, status
 
-class RegisterExistsError(HTTPException):
+class Conflict (HTTPException):
 
     def __init__(self, register: str, detail: str = "já registrado"):
         self.detail = f"{register} {detail}"
 
         super().__init__(status_code=status.HTTP_409_CONFLICT,detail=self.detail)
 
-class RegisterNotFoundError(HTTPException):
+class NotFound (HTTPException):
 
     def __init__(self, register: str, detail: str = "não encontrado"):
         self.detail = f"{register} {detail}"
 
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=self.detail)
 
-class NotPermission (HTTPException):
+class Forbidden (HTTPException):
 
-    def __init__(self, detail: str = "Você não tem permissão para acessar este recurso"):
+    def __init__(self, detail: str = "Sem permissão"):
         self.detail = detail
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=self.detail)
 
 class Unauthorized (HTTPException):
 
-    def __init__(self, detail: str = "Não autentificado"):
+    def __init__(self, detail: str = "Não autenticado"):
         self.detail = detail
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=self.detail)
 
@@ -31,3 +31,9 @@ class BadRequest (HTTPException):
     def __init__(self, detail: str = "Erro de requisição"):
         self.detail = detail
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=self.detail)
+
+class BadGateway (HTTPException):
+
+    def __init__(self, detail: str = "Serviço indisponível"):
+        self.detail = detail
+        super().__init__(status_code=status.HTTP_502_BAD_GATEWAY, detail=self.detail)
