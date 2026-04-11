60f874f2bc1406230a30b07e56fc4b6cf3841582
Hugo Mendes <hugogs.mendes@gmail.com>
2026-04-11 00:04:08 -0300

    feat: rate limiter nas rotas de register e login

--- api/routes/user_routes.py
+++ api/routes/user_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status, Response, Request
 from utils.schemas import ResponseUser, RegisterUser, LoginUser, UpdateUser, UpdatePasswordUser
+from app.main import limiter
 from service.user_service import User_Service
 from utils.dependencies import get_user_service, get_current_user
 from utils.cookies import clear_auth_cookies, set_access_cookie, set_refresh_cookie
@@ -7,11 +8,13 @@ from utils.cookies import clear_auth_cookies, set_access_cookie, set_refresh_coo
 user_router = APIRouter(prefix = "/v1", tags = ["user"])
 
 @user_router.post(path = "/register", status_code = status.HTTP_201_CREATED, response_model = ResponseUser)
-async def register_user (body: RegisterUser, service: User_Service = Depends(get_user_service)):
+@limiter.limit("10/minute")
+async def register_user (request: Request, body: RegisterUser, service: User_Service = Depends(get_user_service)):
     return await service.create_user(body)
 
 @user_router.post(path = "/login", status_code = status.HTTP_200_OK)
-async def login (response: Response, body: LoginUser, service: User_Service = Depends(get_user_service)):
+@limiter.limit("10/minute")
+async def login (request: Request, response: Response, body: LoginUser, service: User_Service = Depends(get_user_service)):
     res = await service.login(body)
     set_access_cookie(response, res.get("access_token"))
     set_refresh_cookie(response, res.get("refresh_token"))
--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func = get_remote_address)
 
 app = FastAPI(
     title = "Lenos IA",
     version = "1.0.0"
 )
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 from api.routes.user_routes import user_router
 from api.routes.question_routes import question_router
