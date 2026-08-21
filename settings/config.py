45f88e2409d564ad94ae8f3d53dab71e137d6070
Hugo Mendes <hugogs.mendes@gmail.com>
2026-08-21 10:45:52 -0300

    .env.example atualizado

--- settings/config.py
+++ settings/config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings (BaseSettings):
 	model_config = SettingsConfigDict(env_file = ".env", env_file_encoding = "utf-8")
 
-	YOUTUBE_API_KEY: str
 	GEMINI_API_KEY: str
 	PEPPER: str
 	ALEMBIC_URL: str | None = None
